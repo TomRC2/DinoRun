c9d5d5967b6a2e4c6c7cb71b568321eae56c46a3
TomRC2 <tomyulanosky@gmail.com>
2024-10-02 00:47:37 -0300

    Finished Game

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("DinoScene");
+        
+    }
+    public void QuitGame()
+    {
+        
+        Application.Quit();
+    }
+
+
+}
